4506233cce3f9748d600e5b75a6eaa9672cb679a
Daniel ferreira nunes <segundacontafd@gmail.com>
2023-11-27 12:05:40 -0300

    make the component for login

--- src/app/components/login/navbar.tsx
+++ src/app/components/login/navbar.tsx
@@ -2,16 +2,14 @@ import Image from 'next/image'
 
 export default function Navbar() {
     const navbarStyle = {
-        backgroundColor: '#ffffff',
-        height: '60%',
+        backgroundColor: '#333333',
+        height: '80px',
         width: '100%'
     }
 
     return (
         <main style={navbarStyle}>
-            <div>
-                <h1>Navbar</h1>
-            </div>
+            <h1>Navbar</h1>
         </main>
     )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Image from 'next/image'
 import Navbar from './components/login/navbar'
+import LoginConteiner from './components/login/LoginConteiner'
 
 export default function Home() {
   return (
-    <main>
+    <>
       <Navbar/>
-    </main>
+      <LoginConteiner/>
+    </>
   )
 }
